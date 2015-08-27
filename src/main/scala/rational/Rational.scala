bb039c251eb2870726e4c746bebf8da257f62cf2
ktarow <ktarou569@gmail.com>
2015-08-28 00:43:54 +0900

    add + method and * method.

--- src/main/scala/rational/Rational.scala
+++ src/main/scala/rational/Rational.scala
@@ -2,18 +2,29 @@ package com.example.trainingscala.rational
 
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  val number: Int = n
-  val denom: Int = d
-
+  private val g = gcd(n.abs, d.abs)
+  val number: Int = n / g
+  val denom: Int = d / g
   def this(n: Int) = this(n, 1)
 
-  override def toString = n + "/" + d
+  override def toString = number+ "/" + denom
 
-  def add(that: Rational): Rational = {
+  def + (that: Rational): Rational = {
     new Rational(
       number * that.denom + that.number * denom,
       denom * that.denom
     )
+  }
+
+  def * (that: Rational) : Rational = {
+    new Rational(
+      number * that.number,
+      denom * that.denom
+    )
+  }
+
+  private def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a else gcd(b, a % b)
   }
 
   def lessThan(that: Rational) =
