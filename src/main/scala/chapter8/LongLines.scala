7693b533dd33bf18c2e11b757351bffb8d6b3bd8
ktarow <ktarou569@gmail.com>
2015-08-30 10:35:39 +0900

    modify: refactoring

--- src/main/scala/chapter8/LongLines.scala
+++ src/main/scala/chapter8/LongLines.scala
@@ -3,14 +3,14 @@ import scala.io.Source
 
 object LongLines {
   def processFiles(filename: String, width: Int) = {
+    def processLine(line: String): Unit = {
+      if (line.length > width) {
+        println(filename + " : " + line.trim)
+      }
+    }
+
     val source = Source.fromFile(filename)
     for (line <- source.getLines())
-      processLine(filename, width, line)
-  }
-
-  private def processLine(filename: String, width: Int, line: String): Unit = {
-    if (line.length > width) {
-      println(filename + " : " + line.trim)
-    }
+      processLine(line)
   }
 }
