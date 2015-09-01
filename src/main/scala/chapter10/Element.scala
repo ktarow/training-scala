01e493c6a99b477e7f06fc83b456d452494d6273
ktarow <ktarou569@gmail.com>
2015-09-01 22:26:45 +0900

    add: chapter10.11,12

--- src/main/scala/chapter10/Element.scala
+++ src/main/scala/chapter10/Element.scala
@@ -12,4 +12,18 @@ abstract class Element {
    */
   def height: Int = contents.length
   def width: Int = if (height == 0) 0 else contents(0).length
+
+  def above(that: Element): Element =
+    new ArrayElement(this.contents ++ that.contents)
+
+  def beside(that: Element): Element = {
+    val contents = new Array[String](this.contents.length)
+    new ArrayElement(
+      for (
+        (line1, line2) <- this.contents zip that.contents
+      ) yield line1 + line2
+    )
+  }
+
+  override def toString = contents mkString "\n"
 }
--- src/main/scala/chapter10/LineElement.scala
+++ src/main/scala/chapter10/LineElement.scala
@@ -3,7 +3,8 @@ package chapter10
 class LineElement(s: String) extends Element {
   // 具象メンバーを実装する場合には，override修飾子は必須
   // 抽象メンバーは任意
-  val contents = Array(s)
   override def width = s.length
   override def height = 1
+
+  val contents = Array(s)
 }
