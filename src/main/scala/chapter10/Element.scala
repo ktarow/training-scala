f203f37ed0a762449d64dd0fd85b47450d9fef9f
ktarow <ktarou569@gmail.com>
2015-09-02 01:04:32 +0900

    add chapter10.15

--- src/main/scala/chapter10/Element.scala
+++ src/main/scala/chapter10/Element.scala
@@ -1,5 +1,27 @@
 package chapter10
 
+object Element {
+  private class ArrayElement(val contents: Array[String]) extends Element {
+  }
+  private class LineElement(s: String) extends Element {
+    override def width = s.length
+    override def height = 1
+    val contents = Array(s)
+  }
+  private class UniformElement(ch: Char, override val width: Int, override val height: Int) extends Element {
+    private val line = ch.toString * width
+    def contents = Array.fill(height)(line)
+  }
+
+  def elem(contents: Array[String]): Element =
+    new ArrayElement(contents)
+  def elem(ch: Char, width: Int, height: Int): Element =
+    new UniformElement(ch, width, height)
+  def elem(line: String): Element =
+    new LineElement(line)
+}
+
+import Element.elem
 // 抽象クラス
 // インスタンスを作成することはできない
 abstract class Element {
@@ -11,18 +33,38 @@ abstract class Element {
     一般に副作用を起こすメソッドには空カッコを，副作用のないメソッドはパラメータなしメソッドを書く
    */
   def height: Int = contents.length
-  def width: Int = if (height == 0) 0 else contents(0).length
+  def width: Int = contents(0).length
 
-  def above(that: Element): Element =
-    new ArrayElement(this.contents ++ that.contents)
-
+  def above(that: Element): Element = {
+    val this1 = this widen that.width
+    val that1 = that widen this.width
+    elem(this1.contents ++ that1.contents)
+  }
   def beside(that: Element): Element = {
-    val contents = new Array[String](this.contents.length)
-    new ArrayElement(
-      for (
-        (line1, line2) <- this.contents zip that.contents
-      ) yield line1 + line2
+    val this1 = this heighten that.height
+    val that1 = that heighten this.height
+    elem(
+      for ((line1, line2) <- this1.contents zip that1.contents)
+        yield line1 + line2
     )
+  }
+
+  def widen(w: Int): Element = {
+    if (w <= width) this
+    else {
+      val left = elem(' ', (w - width)/2 , height)
+      val right = elem(' ', w- width - left.width, height)
+      left beside this beside right
+    }
+  }
+
+  def heighten(h: Int): Element = {
+    if (h <= height) this
+    else {
+      val top = elem(' ', width, (h - height)/2)
+      val bot = elem(' ', width, h - height - top.height)
+      top above this above bot
+    }
   }
 
   override def toString = contents mkString "\n"
