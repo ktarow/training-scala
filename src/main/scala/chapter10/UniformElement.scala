5781ef41a97c45353bda2b44c7f4f730fb3e24f4
ktarow <ktarou569@gmail.com>
2015-09-02 01:06:43 +0900

    delete: *Element.scala

--- src/main/scala/chapter10/UniformElement.scala
+++
@@ -1,6 +0,0 @@
-package chapter10
-
-class UniformElement(ch: Char, override val width: Int, override val height: Int) extends Element {
-  private val line = ch.toString * width
-  def contents = Array.fill(height)(line)
-}
